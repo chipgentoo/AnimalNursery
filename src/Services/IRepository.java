1fb0bfbd6a2995811730c3bb20d134a48b80571c
chipgentoo <chip.gentoo@gmail.com>
2023-12-06 05:08:43 +0300

    Интерфейс рапозитория животных

--- src/Services/IRepository.java
+++ src/Services/IRepository.java
@@ -1,2 +1,11 @@
-package Services;public interface IRepository {
+package Services;
+
+import java.util.List;
+
+public interface IRepository <T> {
+    List<T> getAll();
+    T getById(int id);
+    int create(T item);
+    int update(T item);
+    void delete (int item);
 }
