78c0d1cfdc5e700e2f3be0bef0e4c920820e30d8
chipgentoo <chip.gentoo@gmail.com>
2023-12-06 20:23:24 +0300

    Интерфейс для класса ConsoleView

--- src/UserInterface/View.java
+++ src/UserInterface/View.java
@@ -1,2 +1,10 @@
-package UserInterface;public interface View {
+package UserInterface;
+
+import java.util.List;
+
+public interface View<T>{
+    String getName();
+    String getBirthday();
+    <U> void printAll (List <U> list, Class <U> clazz);
+    void showMessage (String s);
 }
