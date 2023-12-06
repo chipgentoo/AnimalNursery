86609100f00fb519810de0250843029d0e15fd5f
chipgentoo <chip.gentoo@gmail.com>
2023-12-06 05:16:24 +0300

    Добавленно: Модель и Репозиторий

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import Model.Pet;
+import Services.IRepository;
+import Services.PetRepository;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //
+
+        IRepository <Pet> myFarm = new PetRepository();
     }
 }
